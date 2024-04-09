89efd687bc29506d1201f17468dfc845db6cb6f2
leeyiyii <yiwei.li21@student.xjtlu.edu.cn>
2024-04-09 16:43:21 +0800

    TEST

---
+++ src/main/java/com/example/cpt20216/Cpt20216Application.java
@@ -0,0 +1,13 @@
+package com.example.cpt20216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpt20216Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cpt20216Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cpt20216/Cpt20216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpt20216;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpt20216ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
